abced3b04caff5e57c89fc9cc46a2d1339585f77
3A <524019351@qq.com>
2019-03-03 17:09:00 +0800

    抽签搞定

---
+++ chouqian.js
@@ -0,0 +1,23 @@
+var info = document.getElementById("info");
+
+function rand(num)
+{
+	return Math.floor(Math.random()*num+1);
+}
+
+function chouqian()
+{
+	var num = document.getElementById("number").value;
+	if(num=="")
+	{
+		mui.alert('请输入学号末尾', '抽签结果');
+	}
+	else
+	{
+		num = parseInt(num);
+		var result = rand(num);
+		mui.alert(result.toString(), '抽签结果' /*, function() {
+			info.innerText = '通知区可以显示内容';
+		}*/);
+	}
+}
